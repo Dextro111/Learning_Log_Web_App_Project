e8a0546cf08e7eff8f0042718d3cccf17b07357b
dextro111 <destinybishop49@gmail.com>
2024-07-30 22:24:36 +0100

    Updated build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python3.9 manage.py collectstatic --no-input
-python3.9 manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
