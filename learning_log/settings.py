11c167f88aefe641e614cdd9e0a3168b45f0017a
dextro111 <destinybishop49@gmail.com>
2024-08-14 12:34:03 +0100

    Reverted to Render

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/"assets"#os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR/"assets"     #os.path.join(BASE_DIR, "staticfiles")#
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (os.path.join((BASE_DIR, "static"),))
+#STATICFILES_DIRS = (os.path.join((BASE_DIR, "static"),))
 django_heroku.settings(locals())
 
 # Default primary key field type
