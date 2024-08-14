beddd58fa1632695f1dd86164852d2cb0114acc9
dextro111 <destinybishop49@gmail.com>
2024-08-14 11:25:12 +0100

    made changes to STATICFILESDIRS

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")#BASE_DIR/"assets"
+STATIC_ROOT = BASE_DIR/"assets"#os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join((BASE_DIR, "static"),))
 django_heroku.settings(locals())
